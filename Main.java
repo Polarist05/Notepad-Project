a2324700fd9a34a4e4cdd880a7c6789b2e3d9665
Attaphon Plianprasert <plianattaphon@gmail.com>
2022-05-05 21:07:12 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.beans.EventHandler;
-
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -9,6 +11,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -22,6 +25,8 @@ enum FileMenu{
 };
 
 public class Main extends Application{
+    private static Alert alert;
+
     public static void main(String[] args) {
         launch();
     }
@@ -30,16 +35,15 @@ public class Main extends Application{
         //Menu
         EnumMenu fileMenu = new EnumMenu(FileMenu.class,"File");
         MenuBar menuBar = new MenuBar(fileMenu.getMenu());
-        
+        alert = new Alert(AlertType.WARNING,"Read-Only File!", ButtonType.CLOSE);
         
         //set Event
-        fileMenu.getMenuItems()[FileMenu.New.ordinal() ].setOnAction(this::onClickSave);
-        fileMenu.getMenuItems()[FileMenu.Open.ordinal()].setOnAction(this::onClickSave);
+        fileMenu.getMenuItems()[FileMenu.New.ordinal() ].setOnAction(this::onClickNew);
+        fileMenu.getMenuItems()[FileMenu.Open.ordinal()].setOnAction(this::onClickOpen);
         fileMenu.getMenuItems()[FileMenu.Save.ordinal()].setOnAction(this::onClickSave);
-        fileMenu.getMenuItems()[FileMenu.SaveAs.ordinal()].setOnAction(this::onClickSave);
-        fileMenu.getMenuItems()[FileMenu.Exit.ordinal()].setOnAction(this::onClickSave);
-        
-        
+        fileMenu.getMenuItems()[FileMenu.SaveAs.ordinal()].setOnAction(this::onClickSaveAs);
+        fileMenu.getMenuItems()[FileMenu.Exit.ordinal()].setOnAction(this::onClickExit);
+
         //BorderPane
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(menuBar);
@@ -51,10 +55,9 @@ public class Main extends Application{
         stage.show();
     }
     
-
     //Event Action
     public void onClickNew(ActionEvent event){
-        
+        //alert.show();
     }
 
     public void onClickOpen(ActionEvent event){
